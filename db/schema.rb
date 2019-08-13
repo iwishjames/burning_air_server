0d4af999fe062d421b906eb7c5f81ac579bea8e2
Lucy Ogden-Doyle <lucyod10@gmail.com>
2019-08-14 09:43:33 +1000

    added is_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_065044) do
+ActiveRecord::Schema.define(version: 2019_08_13_234211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_065044) do
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
   end
 
 end
